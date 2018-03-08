46ed66e008ce84da49b379e41d3e02ebef94a058
Paul C. George <georgepaul@live.ca>
2018-03-08 18:53:20 -0500

    Update config.php

--- src/config.php
+++ src/config.php
@@ -4,6 +4,6 @@ $config['db']['host'] = '127.0.0.1';
 $config['db']['port'] = '3306';
 $config['db']['username'] = 'root';
 $config['db']['password'] = '8751qwer';
-$config['db']['dbname'] = 'forums';
+$config['db']['dbname'] = 'forumsdev';
 
 $config['fullUnicode'] = true;
