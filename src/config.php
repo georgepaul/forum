3cf275ba85bd212923e6526c57a3df393316796a
Your Name <you@example.com>
2018-05-10 19:03:08 -0400

    add gitignore

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config['db']['host'] = '127.0.0.1';
-$config['db']['port'] = '3306';
-$config['db']['username'] = 'root';
-$config['db']['password'] = '8751qwer';
-$config['db']['dbname'] = 'forumsdev';
-
-$config['fullUnicode'] = true;
