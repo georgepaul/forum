cff8d358802ad9549c309a7d17c4204d96f3d601
Paul C. George <georgepaul@live.ca>
2018-05-18 08:19:54 -0400

    Delete config.php

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Auto update test
-$config['db']['host'] = '127.0.0.1';
-$config['db']['port'] = '3306';
-$config['db']['username'] = 'root';
-$config['db']['password'] = '8751qwer';
-$config['db']['dbname'] = 'forums';
-
-$config['fullUnicode'] = true;
