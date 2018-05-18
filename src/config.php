b3dd47a9a28070b78cf969e0413a01f86c88db4a
Your Name <you@example.com>
2018-05-18 08:25:03 -0400

    +config.php

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 // Auto update test
-$config['db']['host'] = '127.0.0.1';
+$config['db']['host'] = '165.227.33.155';
 $config['db']['port'] = '3306';
-$config['db']['username'] = 'root';
+$config['db']['username'] = 'pgeorge';
 $config['db']['password'] = '8751qwer';
 $config['db']['dbname'] = 'forums';
 
